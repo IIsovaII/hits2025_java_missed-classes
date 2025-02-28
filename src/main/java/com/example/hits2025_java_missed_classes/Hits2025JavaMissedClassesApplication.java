6db8f775c6242ffb3046d7f9c3e96d329677df83
Sofya Kovalenko <sofyakovalenko2005610@gmail.com>
2025-02-28 22:05:01 +0700

    Initial commit

---
+++ src/main/java/com/example/hits2025_java_missed_classes/Hits2025JavaMissedClassesApplication.java
@@ -0,0 +1,13 @@
+package com.example.hits2025_java_missed_classes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hits2025JavaMissedClassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hits2025JavaMissedClassesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hits2025_java_missed_classes/Hits2025JavaMissedClassesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hits2025_java_missed_classes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hits2025JavaMissedClassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
