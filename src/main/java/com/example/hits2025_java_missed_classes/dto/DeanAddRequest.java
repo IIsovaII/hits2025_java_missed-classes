2d276d52869f938922fa28c9e74ce2a30455f21c
RICRamiel <tardis40type@gmail.com>
2025-03-07 01:11:36 +0700

    added one endpoint of administration, tried to use preAuthorize, but still can't understand how it works

---
+++ src/main/java/com/example/hits2025_java_missed_classes/dto/DeanAddRequest.java
@@ -0,0 +1,12 @@
+package com.example.hits2025_java_missed_classes.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeanAddRequest {
+    private String userId;
+    private String faculty;
+}
