d185ba138d2a31b8b161f436855f4acee50ac1f8
Vasilii Polushkin <kvvarta@yandex.ru>
2025-03-08 19:49:58 +0700

    fixes

--- src/main/java/com/example/hits2025_java_missed_classes/exception/ErrorResponseDto.java
+++ src/main/java/com/example/hits2025_java_missed_classes/exception/ErrorResponseDto.java
@@ -1,8 +1,11 @@
 package com.example.hits2025_java_missed_classes.exception;
 
+import lombok.Data;
+
 import java.util.Collections;
 import java.util.List;
 
+@Data
 public class ErrorResponseDto {
     private List<String> errors;
 
--- src/main/java/com/example/hits2025_java_missed_classes/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/hits2025_java_missed_classes/exception/GlobalExceptionHandler.java
@@ -1,14 +1,16 @@
 package com.example.hits2025_java_missed_classes.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/*
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+class GlobalExceptionHandler {
     @ExceptionHandler(RequestDeniedException.class)
-    public ResponseEntity<ErrorResponseDto> handleRequestDeniedException(RequestDeniedException ex) {
-        return new ResponseEntity<>(new ErrorResponseDto(ex.getMessage()), HttpStatus.FORBIDDEN);
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String employeeNotFoundHandler(RequestDeniedException ex) {
+        return ex.getMessage();
     }
-}
+}*/
